136ff946cdaa9c729d9608df0de068cc0834f223
hye-0000 <71963159+hye-0000@users.noreply.github.com>
2022-01-15 10:39:41 +0900

    Add files via upload

---
+++ 0115/example/TextBlock.java
@@ -0,0 +1,15 @@
+package example;
+
+public class TextBlock {
+
+	public static void main(String[] args) {
+
+		String textBlock = """"
+				Hello,
+				Lee,
+				How r U""";"
+		System.out.println(textBlock);
+
+	}
+
+}
